a3e2a833ab0c443153b7d7907322506fb211f28f
Ashiqur <ashiqur2812@gmail.com>
2025-05-01 10:57:02 +0600

    practiced object

--- src/1.5.ts
+++ src/1.5.ts
@@ -1,1 +1,19 @@
 // Reference Type --> Object
+
+const user : {
+    firstName : string,
+    middleName?: string, // optional type
+    lastName: string,
+    learn: 'programming Hero', // literal types
+    isMarried: boolean,
+    readonly company: string
+} = {
+    firstName: 'Muhammad',
+    // middleName: 'Jason',
+    lastName: 'Roy',
+    learn: 'programming Hero',
+    isMarried: false,
+    company: 'softvene agency'
+}
+
+// user.company = 'PH'
