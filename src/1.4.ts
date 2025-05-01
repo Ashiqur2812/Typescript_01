366854fe33c3765842b28e9889a2561dd876ae6e
Ashiqur <ashiqur2812@gmail.com>
2025-05-01 10:46:42 +0600

    practiced data type

---
+++ src/1.4.ts
@@ -0,0 +1,21 @@
+// Basic Data Type
+
+// string
+let firstName: string = 'Roy';
+// number
+let roll: number = 123;
+// boolean
+let isAdmin: boolean = true;
+// undefined
+let x: undefined = undefined;
+// null
+let y: null = null;
+// Array
+const friends: string[] = ['Roy', 'Jason'];
+const numbers: [number, string, boolean] = [1, '3', true];
+
+// numbers.push(34)
+
+
+let d: string;
+d = '123';
